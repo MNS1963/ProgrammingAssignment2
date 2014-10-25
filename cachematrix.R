ff4f480b798ff36e1323b06444f3331ab1e15f97
Mel Schmitz <Mel.Schmitz@sbcglobal.net>
2014-10-25 15:12:10 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cache the inverse if a matrix,
+## as part of Coursera R programming course.
 
 ## Write a short comment describing this function
 
