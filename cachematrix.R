7ccd48e88f5d26cd40e0877cd2a280be1310a0e6
Mel Schmitz <Mel.Schmitz@sbcglobal.net>
2014-10-26 13:48:45 -0500

    Update cachematrix.R
    Added comments to explain purpose of program and functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,22 @@
-## Cache the inverse if a matrix,
-## as part of Coursera R programming course.
+## This progam contains 2 functions to cache the inverse of a matrix.
+## If the inversed matrix has been preiously cached it will be used.
+## If not, the inverse of the matrix will be calculated.
+## This program first drafted on 10/25/2014 as part of Coursera R programming course.
+## This program last updated on 10/26/2014.
 
-## Write a short comment describing this function
+## The makeCacheMatrix determines where cacheSolve has been previously called.
+## If yes, get anser from cache(memory).  If no call function cacheSolve.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        If(!isNull) {
+                ## return value of M from cache
+                return(m)
+        }
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function calculates the inverse of a matrix using the R solve function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        solve(m)  ## Return a matrix that is the inverse of 'x'
 }
